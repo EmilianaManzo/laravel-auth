e2ea2f1f4bfd2dd44fd64434bf2f33f8210e5256
EmilianaManzo <emiliana.ma96@gmail.com>
2024-05-21 15:20:43 +0200

    [add] cartelle in view, controllers, e sistemato rotte

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>admin homepage</h1>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.guest')
+
+@section('content')
+
+<h1>guest homepage</h1>
+@endsection
+
